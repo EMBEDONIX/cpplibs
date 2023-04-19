23b5595dcf238df9e48d7584154c7238659b0b17
Saeid Yazdani <saeid@embedonix.com>
2023-04-20 00:11:25 +0200

    First commit

---
+++ Embedonix/LibInfo.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Saeid Yazdani on 4/19/2023.
+//
+
+#include "LibInfo.h"
+
+namespace embedonix {
+  std::string libraryVersion() {return "0.1.0";}
+  std::string libraryName(){return "Embedonix Simple Library";};
+  std::string libraryDescription() {return "A Collection of simple but yet"
+                                           " powerfull libraries for c++"
+                                           " programmers!";};
+}
---
+++ Embedonix/LibInfo.h
@@ -0,0 +1,12 @@
+//
+// Created by Saeid Yazdani on 4/19/2023.
+//
+
+#pragma once
+
+#include <string>
+namespace embedonix {
+  std::string libraryVersion();
+  std::string libraryName();
+  std::string libraryDescription();
+}
