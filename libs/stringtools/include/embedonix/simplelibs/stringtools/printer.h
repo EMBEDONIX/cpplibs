0c1362650837acdcb18f086f032f4a254fbb7f2f
Saeid Yazdani <saeid@embedonix.com>
2023-05-01 23:00:02 +0200

    Simplified printer.h

--- libs/stringtools/include/embedonix/simplelibs/stringtools/printer.h
+++ libs/stringtools/include/embedonix/simplelibs/stringtools/printer.h
@@ -17,37 +17,27 @@ namespace embedonix::simplelibs::stringtools {
  * printing
  * @param container Container to print
  * @param separator A Character or string to separate each element
- * @param addNewLineAtEnd Add a new line when done printing
- * @param stream The stream to print into
+ * @param addNewLine Add a new line when done printing
+ * @param os The stream to print into
  */
-template
-    <
-        template<typename...> typename CONTAINER,
-        typename ELEMENT_TYPE,
-        typename SEPARATOR = char // need the hint for default parameter type
-    >
-void print_container(const CONTAINER<ELEMENT_TYPE>& container,
-                     SEPARATOR separator = ' ',
-                     bool addNewLineAtEnd = true,
-                     std::ostream& stream = std::cout) {
 
-  // Keep track of how many elements have been printed out
-  typename CONTAINER<ELEMENT_TYPE>::size_type index = 0;
-  // we do not want to print separator after last element, so this is the stop
-  typename CONTAINER<ELEMENT_TYPE>::size_type lastIndex = container.size() - 1;
+template<class CONTAINER, typename DELIMITER = char>
+void print_container(const CONTAINER& container, DELIMITER separator = ' ',
+                     bool addNewLine = true,
+                     std::ostream& os = std::cout) {
+  auto it = std::begin(container);
+  auto end = std::end(container);
 
-  for (const auto& element: container) {
-    stream << element;
-    if (index < lastIndex) { // If not the last element, add separator
-      stream << separator;
+  if (it != end) { // There is at least one element available :)
+    os << *it; // Print first element
+    for (++it; it != end; ++it) { // Print the rest, if available
+      os << separator << *it;
     }
-    index++;
+    if(addNewLine) {
+      os << std::endl;
+    }
   }
-
-  if (addNewLineAtEnd) {
-    stream << std::endl;
-  }
-
 }
+
 
 } // End namespace embedonix::simplelibs::stringtools
