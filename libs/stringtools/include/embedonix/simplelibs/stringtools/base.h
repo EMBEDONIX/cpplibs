2b56485edfc5e17db02d0974e9a7b876a87b1bfd
Saeid Yazdani <saeid@embedonix.com>
2023-05-01 23:24:52 +0200

    Added base.h

---
+++ libs/stringtools/include/embedonix/simplelibs/stringtools/base.h
@@ -0,0 +1,13 @@
+//
+// Created by Saeid Yazdani on 5/1/2023.
+//
+
+#pragma once
+
+#include <type_traits>
+
+namespace embedonix::simplelibs::stringtools {
+
+// TODO Find a concept that only allows printable object to be used in printer.h
+
+}
