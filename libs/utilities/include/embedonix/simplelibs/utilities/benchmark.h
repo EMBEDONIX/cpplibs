eb24bdbbcbcd196be79a4de5925d3dbd6cb5f738
Saeid Yazdani <saeid@embedonix.com>
2023-12-23 23:20:03 +0100

    Updated docs

--- libs/utilities/include/embedonix/simplelibs/utilities/benchmark.h
+++ libs/utilities/include/embedonix/simplelibs/utilities/benchmark.h
@@ -25,4 +25,28 @@ namespace embedonix::simplelibs::utilities::benchmark::measure {
         return std::chrono::duration_cast<std::chrono::microseconds>
                 (std::chrono::high_resolution_clock::now() - startTime).count();
     }
+
+    /**
+    * Measures average execution time of a function for given number
+    * of executions in microseconds
+    * @tparam F Function Type
+    * @tparam Args Arguments Type
+    * @param runTimes Number of times to execute the function
+    * @param func Function name
+    * @param args Arguments to the function
+    * @return Average duration it took to execute @b{func} for
+    */
+    template<typename F, typename... Args>
+    double function_average_execution_time(size_t runTimes, F func, Args &&... args) {
+        auto sum = 0.0;
+        for (size_t i = 0; i < runTimes; ++i) {
+            auto startTime =
+                    std::chrono::high_resolution_clock::now();
+            // Execute the function
+            func(std::forward<Args>(args)...);
+            sum += std::chrono::duration_cast<std::chrono::microseconds>
+                    (std::chrono::high_resolution_clock::now() - startTime).count();
+        }
+        return (sum / runTimes);
+    }
 }
