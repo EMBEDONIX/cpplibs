761c5b02a157b82d0bf226df240660dbca71ab98
Saeid Yazdani <saeid@embedonix.com>
2023-12-23 22:10:32 +0100

    Added more benchmarks

--- examples/fileio/fileio.cpp
+++ examples/fileio/fileio.cpp
@@ -20,21 +20,26 @@ void sleepFunction(int milli) {
 
 void read_file_saya(std::string path) {
     volatile auto content = embedonix::simplelibs::fileio
-            ::readers::read_file_bytes(path);
+    ::readers::read_file_bytes(path);
 //    sleepFunction(100);
 }
 
-void read_file_mila(std::string path, std::vector<std::byte>& buffer) {
+void read_file_mila(std::string path, std::vector<std::byte> &buffer) {
     volatile auto result = embedonix::simplelibs::fileio::readers::read_file_bytes_caller_alloc(
             path, buffer);
-    if(!result) {
+    if (!result) {
         exit(1);
     }
 
 //    sleepFunction(100);
 }
 
-int main(int argc, char** argv) {
+void read_file_string(std::string path) {
+    volatile auto result = embedonix::simplelibs::fileio::readers::
+    read_file(path);
+}
+
+int main(int argc, char **argv) {
 
     // Allocate a big enough buffer for files up to 10MB
     auto buffer = std::vector<std::byte>(11'000'000);
@@ -42,7 +47,7 @@ int main(int argc, char** argv) {
     auto fileToRead = "../resources/random-files/ten-mb-file";
 
     for (size_t i = 0; i < 10; ++i) {
-        std::cout << "mila read: " <<
+        std::cout << "read_file_bytes_caller_alloc read:\t\t" <<
                   embedonix::simplelibs::utilities::benchmark::measure::
                   function_execution_time(read_file_mila,
                                           fileToRead,
@@ -51,9 +56,17 @@ int main(int argc, char** argv) {
     }
 
     for (size_t i = 0; i < 10; ++i) {
-        std::cout << "saya read: " <<
+        std::cout << "read_file_bytes read:\t\t\t\t" <<
                   embedonix::simplelibs::utilities::benchmark::measure::
                   function_execution_time(read_file_saya,
+                                          fileToRead)
+                  << std::endl;
+    }
+
+    for (size_t i = 0; i < 10; ++i) {
+        std::cout << "read_file read:\t\t\t\t\t" <<
+                  embedonix::simplelibs::utilities::benchmark::measure::
+                  function_execution_time(read_file_string,
                                           fileToRead)
                   << std::endl;
     }
