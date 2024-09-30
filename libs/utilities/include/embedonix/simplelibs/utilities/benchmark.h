ac22a96e381c21f76fef5b8633a958e0bc55947e
Saeid Yazdani <saeid@embedonix.com>
2024-09-30 09:58:19 +0200

    Updated timer start/stop Added format_duration

--- libs/utilities/include/embedonix/simplelibs/utilities/benchmark.h
+++ libs/utilities/include/embedonix/simplelibs/utilities/benchmark.h
@@ -51,27 +51,56 @@ namespace embedonix::simplelibs::utilities::benchmark::measure {
     }
 
 
-    /**
-     * Starts a timer for a future use
-     * @tparam Clock Type of the clock to be used
-     * @param timer time_point for the timer
-     */
-    template<typename Clock>
-    void start_timer(std::chrono::time_point<Clock>& timer) {
+        /**
+         * Starts a timer for a future use
+         * @tparam Clock Type of the clock to be used
+         * @param timer time_point for the timer
+         */
+    template<typename Clock = std::chrono::high_resolution_clock>
+    inline void start_timer(std::chrono::time_point<Clock>& timer) noexcept {
         timer = Clock::now();
     }
 
     /**
-     * Stops a perviously set time_point and returns the duration
+     * Stops a previously set time_point and returns the duration
      * @tparam Clock Type of the clock to be used
-     * @tparam Duration Resolution of the duration
+     * @tparam Duration Resolution of the duration to return
      * @param timer The timer object holding the start time
+     * @return The duration from the start time until now
+     */
+    template<typename Clock = std::chrono::high_resolution_clock,
+             typename Duration = std::chrono::microseconds>
+    inline Duration stop_timer(const std::chrono::time_point<Clock>& timer) noexcept {
+        auto end = Clock::now();
+        return std::chrono::duration_cast<Duration>(end - timer);
+    }
+
+    /**
+     * Formats a duration count with unit deduced from Duration type
+     * @tparam Duration Duration type
+     * @param duration Duration object
+     * @param space Adds a space between count and unit
      * @return
      */
-    template<typename Clock, typename Duration = std::chrono::microseconds>
-    double stop_timer(std::chrono::time_point<Clock>& timer) {
-        return std::chrono::duration_cast<Duration>
-         (Clock::now() - timer).count();
+    template <typename Duration>
+    inline
+    std::string format_duration(Duration duration, bool space = false)
+    noexcept {
+        using namespace std::chrono;
+
+        // Extract count value from the duration
+        auto count = duration.count();
+
+        // Format the output based on the duration type
+        if constexpr (std::is_same_v<Duration, milliseconds>) {
+            return std::format("{}{}ms", space ? " " : "", count);
+        } else if constexpr (std::is_same_v<Duration, microseconds>) {
+            return std::format("{}{}us", space ? " " : "", count);
+        } else if constexpr (std::is_same_v<Duration, seconds>) {
+            return std::format("{}{}s", space ? " " : "", count);
+        } else {
+            return std::format("{}{}(unknown unit)", space ? " " : "", count);
+        }
     }
 
 }
